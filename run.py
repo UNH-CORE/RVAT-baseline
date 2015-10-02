8f02e37bad2df444b061c4d5e5204be774ce1019
Pete Bachant <petebachant@gmail.com>
2015-10-02 14:47:25 -0400

    Make run.py a processing script

--- run.py
+++ run.py
@@ -1,29 +1,10 @@
 #!/usr/bin/env python
 """
-This script generates figures from the RVAT baseline experiment.
+This script processes data from the RVAT baseline experiment.
 """
 
-from Modules.plotting import *
-from Modules.processing import *
-
-setpltparams(seaborn=True, latex=False)
-save = True
-show = True
-
-def main():    
-    # List of plots for JoT paper
-    jotplots = ["meancontquiv", "xvorticity", "fpeak_v", "fstrength_v",
-                "uvcont", "uwcont", "Kturbtrans", "kcont", "Kbargraph",
-                "mombargraph"]
-    plotperf(subplots=False, save=save)
-    plotperf(subplots=True, save=save)
-    plotwake(jotplots, save=save, print_analysis=True)
-    plotmultispec(n_band_average=4, save=save, plot_conf_int=True)
-    
-    if show:
-        plt.show()
+import py_rvat_baseline.processing as pr
 
 if __name__ == "__main__":
-    if not os.path.isdir("./Figures"):
-        os.mkdir("Figures")
-    main()
+    pr.batchperf()
+    pr.batchwake()
