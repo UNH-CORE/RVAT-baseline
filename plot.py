cfef7fa8bd62eab03e1d4f1d012b0dc46452f1d1
Pete Bachant <petebachant@gmail.com>
2016-02-09 22:29:18 -0500

    Add more plots and options to plot.py

--- plot.py
+++ plot.py
@@ -7,18 +7,21 @@ import sys
 
 
 jotfigs = ["meancontquiv", "xvorticity", "fpeak_v", "fstrength_v", "uvcont",
-           "uwcont", "Kturbtrans", "kcont", "Kbargraph", "mombargraph"]
+            "uwcont", "Kturbtrans", "kcont", "Kbargraph", "mombargraph"]
+
+wakefigs_all = jotfigs + ["meanu_2tsrs", "meanv_2tsrs", "meanw_2tsrs",
+                          "meanvel_vs_tsr", "k_2tsrs"]
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Create figures from the "
                                      "UNH-RVAT baseline experiment.")
-    parser.add_argument("figures", nargs="?", help="Which figures to create",
-                        choices=jotfigs + ["multispec", "perf"],
-                        default=[])
+    parser.add_argument("figures", nargs="*", help="Which figure to create",
+                        choices=wakefigs_all + ["multispec", "perf", []],
+                        default=None)
     parser.add_argument("--no-subplots", action="store_true",
                         help="Don't use subplots for performance plots")
-    parser.add_argument("--all", "-A", help="Plot all figures",
+    parser.add_argument("--all", "-a", help="Plot all figures",
                         action="store_true", default=False)
     parser.add_argument("--no-errorbars", "-e", help="Do not plot error bars",
                         action="store_true", default=False)
@@ -27,11 +30,14 @@ if __name__ == "__main__":
     parser.add_argument("--savetype", help="Format to save figures",
                         default=".pdf")
     parser.add_argument("--style", help="Matplotlib stylesheet")
-    parser.add_argument("--noshow", help="Do not show figures",
+    parser.add_argument("--no-show", help="Do not show figures",
+                        action="store_true", default=False)
+    parser.add_argument("--verbose", "-v", help="Print analysis",
                         action="store_true", default=False)
     args = parser.parse_args()
 
     if not args.figures and not args.all:
+        parser.print_help()
         sys.exit("No figures chosen to plot")
 
     if args.style:
@@ -49,17 +55,17 @@ if __name__ == "__main__":
 
     if not isinstance(args.figures, list):
         args.figures = [args.figures]
-    wakefigs = [f for f in args.figures if f in jotfigs]
-
+    wakefigs = [f for f in args.figures if f in wakefigs_all]
 
     if "perf" in args.figures or args.all:
-        plotperf(subplots=subplots, save=save, savetype=savetype)
+        plotperf(subplots=subplots, save=save, savetype=savetype,
+                 print_perf=args.verbose)
     if wakefigs or args.all:
         if args.all:
-            wakefigs = jotfigs
-        plotwake(wakefigs, save=save, print_analysis=True)
+            wakefigs = wakefigs_all
+        plotwake(wakefigs, save=save, print_analysis=args.verbose)
     if "multispec" in args.figures or args.all:
         plotmultispec(n_band_average=4, save=save, plot_conf_int=errorbars)
 
-    if not args.noshow:
+    if not args.no_show:
         plt.show()
